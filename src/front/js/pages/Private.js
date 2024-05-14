531eb31dc62ed0eeea3744ab460e3fcff6a33c34
ronalse <157391307+ronalse@users.noreply.github.com>
2024-05-14 17:45:53 +0000

    ronaldo

--- src/front/js/pages/Private.js
+++ src/front/js/pages/Private.js
@@ -3,18 +3,32 @@ import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
 export const Private = () => {
-  const { store, actions } = useContext(Context);
   const [ email, setEmail ] = useState("")
+  
   const navigate = useNavigate();
 
   const token = localStorage.getItem("token");
-
-  setEmail= store.setEmail,
   
   useEffect(() => {
 
-    actions.gettingInfo()
-		
+    const gettingInfo = async () => {
+
+      const response = await fetch(
+        "https://orange-meme-jjjvx4pgr56wf7qq-3001.app.github.dev/private",
+        {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        }
+      );
+  
+      const data = await response.json();
+      setStore({ setEmail: [...getStore().setEmail, data.email] });
+      console.log(data.email, "This is the email of the user")
+      }
+      gettingInfo()
 	  }, [])
 
   return (
